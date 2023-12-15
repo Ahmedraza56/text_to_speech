ffe9b96cbfa43d115de35bdbf739f9e9c4e79dd7
ahmed <ahmedrazy830@gmail.com>
2023-12-15 11:40:14 +0500

    intital commit

--- text_to_speech.py
+++ text_to_speech.py
@@ -7,17 +7,27 @@ app = Flask(__name__)
 
 # Function to convert text to speech
 def text_to_speech(text):
-    tts = gTTS(text)
-    audio_buffer = BytesIO()
-    tts.write_to_fp(audio_buffer)
-    audio_buffer.seek(0)
-    return audio_buffer
+    try:
+        tts = gTTS(text)
+        audio_buffer = BytesIO()
+        tts.write_to_fp(audio_buffer)
+        audio_buffer.seek(0)
+        return audio_buffer
+    except Exception as e:
+        # Handle the exception (you can log it, display an error message, etc.)
+        print(f"Error in text_to_speech: {str(e)}")
+        return None
 
 # Function to set download link for audio file
 def get_audio_download_link(buffer, filename="audio.mp3"):
-    buffer.seek(0)
-    b64_data = base64.b64encode(buffer.read()).decode()
-    return f'data:audio/mp3;base64,{b64_data}'
+    try:
+        buffer.seek(0)
+        b64_data = base64.b64encode(buffer.read()).decode()
+        return f'data:audio/mp3;base64,{b64_data}'
+    except Exception as e:
+        # Handle the exception (you can log it, display an error message, etc.)
+        print(f"Error in get_audio_download_link: {str(e)}")
+        return None
 
 # Index route
 @app.route('/', methods=['GET', 'POST'])
@@ -25,13 +35,21 @@ def index():
     if request.method == 'POST':
         user_input_text = request.form.get('user_input_text')
         if user_input_text:
-            # Call function to convert text to speech
-            audio_buffer = text_to_speech(user_input_text)
-            download_link = get_audio_download_link(audio_buffer)
-
-            return render_template('text_to_speech.html', audio_link=download_link)
+            try:
+                # Call function to convert text to speech
+                audio_buffer = text_to_speech(user_input_text)
+                if audio_buffer:
+                    download_link = get_audio_download_link(audio_buffer)
+                    return render_template('text_to_speech.html', audio_link=download_link)
+                else:
+                    # Handle the case where audio conversion failed
+                    return render_template('text_to_speech.html', audio_link=None, error_message="Audio conversion failed.")
+            except Exception as e:
+                # Handle the exception (you can log it, display an error message, etc.)
+                print(f"Error in index route: {str(e)}")
+                return render_template('text_to_speech.html', audio_link=None, error_message="An error occurred.")
     
-    return render_template('text_to_speech.html', audio_link=None)
+    return render_template('text_to_speech.html', audio_link=None, error_message=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
